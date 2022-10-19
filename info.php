0a2b7d9c8336721404cbc80aebe0d9afae32d4b4
Varrt <varrtik@gmail.com>
2022-10-19 07:55:27 +0200

    v0.5.6 - add display inventory info

---
+++ info.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+use PepperAttackBot\Client;
+
+require __DIR__.'/vendor/autoload.php';
+
+echo "--------------------------------------------------------------------------------------\n";
+echo "Start at: " . date("Y-m-d H:i:s")."\n";
+echo "--------------------------------------------------------------------------------------\n";
+
+
+if (!isset($argv[1])) {
+    echo "You must provide an email as first argument.\n";
+    exit;
+}
+
+if (!isset($argv[2])) {
+    echo "You must provide a password as second argument\n";
+    exit;
+}
+
+echo "Account: ". $argv[1].".\n";
+
+$client = new Client();
+$client->login($argv[1], $argv[2]);
+sleep(rand(1,3));
+
+$inventory = $client->getInventory();
+echo "Rations: " . $inventory->getRation() . "\n";
+echo "Potions: ". $inventory->getPotions()."\n";
+echo "Stim: " . $inventory->getStim()."\n";
+
+
+echo "--------------------------------------------------------------------------------------\n";
+echo "End at: " . date("Y-m-d H:i:s")."\n";
+echo "--------------------------------------------------------------------------------------\n";
